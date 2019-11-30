694c0438cd0629a08d6644d8d80c377c2d34980e
wishkind <wishkind@126.com>
2019-11-30 18:13:59 +0800

    chanes some files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+               "template": "index.html",
+               "filename": "./index.html"
+              });
+
+module.exports = { 
+    entry: path.join(__dirname, 'index.js'),
+    output: {
+        path: path.join(__dirname, './'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: /node_moudles/
+            },
+            {
+                test: /\.(css|scss)/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+               test: /\.ts$/,
+               exclude: /node_modules/,
+               use: ['tsc']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.ts']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        htmlWebpackPlugin
+    ],
+    devServer: {
+        hot: true
+    }
+
+}                
